0717bf44736a4ec643fc8c24d14f57786385ddb1
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-10-09 00:29:28 +0300

    cleanup

--- project_01/WeSplit/ContentView.swift
+++ project_01/WeSplit/ContentView.swift
@@ -9,32 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-
-    @State private var tapCount = 0
-    @State private var name = ""
-    @State private var selectedStudent = "Harry"
-
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Your name is \(name)")
-                    Button("Tap Count: \(tapCount)") {
-                        self.tapCount += 1
-                    }
-                    TextField("Enter your name", text: $name)
-                }
-                Section {
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(0 ..< students.count) {
-                            Text(self.students[$0])
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
-        }
+        Text("Hello World")
     }
 }
 
