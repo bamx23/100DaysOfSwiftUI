d6359314811014529eaf0b1c973f13a4b1dd974b
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-10-09 00:03:23 +0300

    add project 01

---
+++ project_01/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Nikolay Volosatov on 10/9/19.
+//  Copyright © 2019 BX23. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
