9d7bafaf02f8d12fd3d6d2adc782d92158432d82
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-10-10 09:54:14 +0300

    add pickers

--- project_01/WeSplit/ContentView.swift
+++ project_01/WeSplit/ContentView.swift
@@ -17,14 +17,27 @@ struct ContentView: View {
     @State private var tipPercentage = 2
 
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
             }
-            Section {
-                Text("$\(checkAmount)")
-            }
+            .navigationBarTitle("WeSplit")
         }
     }
 }
