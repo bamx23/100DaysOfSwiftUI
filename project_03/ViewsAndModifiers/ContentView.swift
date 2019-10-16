3728914dae71f09ee81d3f8c78af35f22118f453
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-10-16 11:09:39 +0300

    play around modifiers and containers

--- project_03/ViewsAndModifiers/ContentView.swift
+++ project_03/ViewsAndModifiers/ContentView.swift
@@ -8,9 +8,89 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black.blur(radius: 2))
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+
     var body: some View {
-        Text("Hello, World!")
+        Text(text)
+            .titleStyle()
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0 ..< self.rows) { row in
+                HStack {
+                    ForEach(0 ..< self.columns) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var useRedText = false
+
+    var body: some View {
+        VStack(spacing: 20) {
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
+                .watermarked(with: "bamx23")
+            GridStack(rows: 3, columns: 3) { r, c in
+                HStack {
+                    Image(systemName: "\(r * 3 + c).circle")
+                    Text("\(r)x\(c)")
+                        .font(.custom("Courier New", size: 10))
+                }
+            }
+        }
     }
 }
 
