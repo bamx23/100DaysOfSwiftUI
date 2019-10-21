4d1ccc40aca4163a5b9dd06b2f75d5a4180f464c
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-10-21 11:46:44 +0300

    implement basic logic

--- project_03_challenge/Kanobu/ContentView.swift
+++ project_03_challenge/Kanobu/ContentView.swift
@@ -8,9 +8,65 @@
 
 import SwiftUI
 
+enum Move {
+    case rock
+    case paper
+    case scissors
+}
+
+extension Move {
+    func name() -> String {
+        switch self {
+        case .rock:
+            return "Rock"
+        case .paper:
+            return "Paper"
+        case .scissors:
+            return "Scissors"
+        }
+    }
+
+    func enemy() -> Move {
+        switch self {
+        case .rock:
+            return .paper
+        case .paper:
+            return .scissors
+        case .scissors:
+            return .rock
+        }
+    }
+
+    static var all: [Move] { [.rock, .paper, .scissors] }
+    static func random() -> Move { all[Int.random(in: 0...2)] }
+}
+
 struct ContentView: View {
+    @State private var appChoice = Move.random()
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+
+    func makeMove(_ move: Move) {
+        let won = shouldWin ? move == appChoice.enemy() : appChoice == move.enemy()
+        score += won ? 1 : -1
+        appChoice = Move.random()
+        shouldWin = Bool.random()
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Score")
+            Text("\(score)")
+            Text("\(appChoice.name())")
+            Text("\(shouldWin ? "Win" : "Lose")")
+            HStack {
+                ForEach(Move.all, id: \.self) { move in
+                    Button(action: { self.makeMove(move) }) {
+                        Text(move.name())
+                    }
+                }
+            }
+        }
     }
 }
 
