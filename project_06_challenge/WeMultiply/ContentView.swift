d300f7703475666f218b84ca9b60750299f57f01
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-10-28 22:01:56 +0300

    add WeMultiply project

---
+++ project_06_challenge/WeMultiply/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeMultiply
+//
+//  Created by Nikolay Volosatov on 10/28/19.
+//  Copyright © 2019 BX23. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
