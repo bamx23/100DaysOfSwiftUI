53f048d00f6d3a50f2d574eeb89f651252ab23bd
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-04 10:43:00 +0300

    add iExpense

---
+++ project_07/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Nikolay Volosatov on 11/4/19.
+//  Copyright © 2019 BX23. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
