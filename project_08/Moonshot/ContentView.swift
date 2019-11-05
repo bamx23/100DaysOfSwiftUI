96a927e5b31cf270926f109d1cdf737bf59cb0d3
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-05 10:16:39 +0300

    geometry reader and image fit

--- project_08/Moonshot/ContentView.swift
+++ project_08/Moonshot/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            GeometryReader { geo in
+                Image("large_image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geo.size.width)
+                    //.resizable()
+            }
+        }
     }
 }
 
