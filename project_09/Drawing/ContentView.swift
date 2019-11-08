3710f4fc404fbb46b84dec519ab746eb603b228e
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-08 12:11:53 +0300

    shape

--- project_09/Drawing/ContentView.swift
+++ project_09/Drawing/ContentView.swift
@@ -8,15 +8,45 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: .init(x: rect.midX, y: rect.minY))
+        path.addLine(to: .init(x: rect.minX, y: rect.maxY))
+        path.addLine(to: .init(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: .init(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        var path = Path()
+        path.addArc(center: .init(x: rect.midX, y: rect.midY),
+                    radius: rect.width / 2,
+                    startAngle: startAngle - rotationAdjustment,
+                    endAngle: endAngle - rotationAdjustment,
+                    clockwise: clockwise)
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: .init(x: 200, y: 100))
-            path.addLine(to: .init(x: 100, y: 300))
-            path.addLine(to: .init(x: 300, y: 300))
-            path.addLine(to: .init(x: 200, y: 100))
+        VStack {
+            Triangle()
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .stroke(Color.blue, lineWidth: 10)
+                .frame(width: 300, height: 300)
         }
-        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
