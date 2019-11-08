14cfd3f952f4199c934b8f8826fa507e1d5db710
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-08 12:29:37 +0300

    insettable shape

--- project_09/Drawing/ContentView.swift
+++ project_09/Drawing/ContentView.swift
@@ -21,32 +21,35 @@ struct Triangle: Shape {
     }
 }
 
-struct Arc: Shape {
+struct Arc: InsettableShape {
     let startAngle: Angle
     let endAngle: Angle
     let clockwise: Bool
+
+    var insetAmount: CGFloat = 0
 
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
         var path = Path()
         path.addArc(center: .init(x: rect.midX, y: rect.midY),
-                    radius: rect.width / 2,
+                    radius: rect.width / 2 - insetAmount,
                     startAngle: startAngle - rotationAdjustment,
                     endAngle: endAngle - rotationAdjustment,
                     clockwise: clockwise)
         return path
     }
+
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Triangle()
-                .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-                .stroke(Color.blue, lineWidth: 10)
-                .frame(width: 300, height: 300)
-        }
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40)
     }
 }
 
