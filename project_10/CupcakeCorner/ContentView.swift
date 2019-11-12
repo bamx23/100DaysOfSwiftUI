af7b26634d8c81977fcb4c5e7f41db6f96326ef7
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-12 23:46:14 +0300

    download Taylor Swift tracks info

--- project_10/CupcakeCorner/ContentView.swift
+++ project_10/CupcakeCorner/ContentView.swift
@@ -8,27 +8,47 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
+struct Response: Codable {
+    var results: [Result]
+}
 
-    @Published var name = "Paul Hudson"
-
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView: View {
+
+    @State private var results: [Result] = []
+
+    func loadData() {
+        let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            let decoder = JSONDecoder()
+            guard error == nil, (response as? HTTPURLResponse)?.statusCode == 200 else {
+                return
+            }
+            guard let data = data, let responseData = try? decoder.decode(Response.self, from: data) else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.results = responseData.results
+            }
+        }
+        task.resume()
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
     }
 }
 
