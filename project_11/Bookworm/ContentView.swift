4e8a9e452d57fe23d64db49fab6c36d0fab1022e
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-15 11:52:30 +0300

    binding

--- project_11/Bookworm/ContentView.swift
+++ project_11/Bookworm/ContentView.swift
@@ -8,9 +8,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    private let onColors = [Color.red, Color.yellow]
+    private let offColors = [Color(white: 0.6), Color(white: 0.4)]
+
     var body: some View {
-        Text("Hello, World!")
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors),
+                               startPoint: .top,
+                               endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 10)
+
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
