8d39dd683d54eceacf647e47f93044a43787c707
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-15 11:43:39 +0300

    add Bookworm

---
+++ project_11/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Nikolay Volosatov on 11/15/19.
+//  Copyright © 2019 BX23. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
