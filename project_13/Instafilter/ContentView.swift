08dfd8986937f52f3076fee2515eb4e57b92578e
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-27 18:31:42 +0300

    add basic Instafilter UI

--- project_13/Instafilter/ContentView.swift
+++ project_13/Instafilter/ContentView.swift
@@ -13,36 +13,84 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var inputImage: UIImage?
     @State private var image: Image?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var filterIntensity = 0.5
+
     @State private var showingImagePicker = false
+
+    let context = CIContext()
 
     func loadImage() {
         guard let inputImage = inputImage else {
             return
         }
-        image = Image(uiImage: inputImage)
+
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-
-            Button("Select Image") {
-                self.showingImagePicker = true
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
             }
-            Button("Save") {
-                guard let image = self.inputImage else {
-                    return
-                }
-                ImageSaver.shared.saveImageToAlbum(image) { error in
-                    if let error = error {
-                        print("Error: \(error)")
+        )
+
+        return NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
                     } else {
-                        print("Saved!")
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intensity)
+                }.padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                        // save the picture
                     }
                 }
             }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
         }
         .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
             ImagePicker(image: self.$inputImage)
