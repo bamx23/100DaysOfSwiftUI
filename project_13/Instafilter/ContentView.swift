92eb0b01d550cc68e617a74a1f3fba3237e239fd
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-11-28 10:06:39 +0300

    add filter selection

--- project_13/Instafilter/ContentView.swift
+++ project_13/Instafilter/ContentView.swift
@@ -13,10 +13,11 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var inputImage: UIImage?
     @State private var image: Image?
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     @State private var filterIntensity = 0.5
 
     @State private var showingImagePicker = false
+    @State private var showingFilterSheet = false
 
     let context = CIContext()
 
@@ -31,7 +32,16 @@ struct ContentView: View {
     }
 
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
 
         guard let outputImage = currentFilter.outputImage else { return }
 
@@ -39,6 +49,11 @@ struct ContentView: View {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
         }
+    }
+
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 
     var body: some View {
@@ -79,7 +94,7 @@ struct ContentView: View {
 
                 HStack {
                     Button("Change Filter") {
-                        // change filter
+                        self.showingFilterSheet = true
                     }
 
                     Spacer()
@@ -94,6 +109,18 @@ struct ContentView: View {
         }
         .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
             ImagePicker(image: self.$inputImage)
+        }
+        .actionSheet(isPresented: $showingFilterSheet) {
+            ActionSheet(title: Text("Select a filter"), buttons: [
+                .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                .cancel()
+            ])
         }
     }
 }
