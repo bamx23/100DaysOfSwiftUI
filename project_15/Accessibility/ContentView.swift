7edea6255f4e734f2683ee12eccab676bdbbf10f
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-12-15 12:12:17 +0300

    add accessibility for controls

--- project_15/Accessibility/ContentView.swift
+++ project_15/Accessibility/ContentView.swift
@@ -24,6 +24,8 @@ struct ContentView: View {
     ]
 
     @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var estimate = 25.0
+    @State private var rating = 3
 
     var body: some View {
         VStack {
@@ -36,6 +38,8 @@ struct ContentView: View {
                 .onTapGesture {
                     self.selectedPicture = Int.random(in: 0...3)
                 }
+
+
             VStack {
                 Text("Your score is")
                 Text("1000")
@@ -43,6 +47,16 @@ struct ContentView: View {
             }
             .accessibilityElement(children: .ignore)
             .accessibility(label: Text("Your score is 1000"))
+
+
+            Slider(value: $estimate, in: 0...50)
+                .padding()
+                .accessibility(value: Text("\(Int(estimate))"))
+
+
+            Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+                .accessibility(label: Text("Rate our service"))
+                .accessibility(value: Text("\(rating) out of 5"))
         }
     }
 }
