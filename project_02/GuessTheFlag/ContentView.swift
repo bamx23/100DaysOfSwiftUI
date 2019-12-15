3a9ff363f3b523d130610fd4d76444bb9a8ecf5a
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-12-15 12:22:57 +0300

    guess the flag accessibility

--- project_02/GuessTheFlag/ContentView.swift
+++ project_02/GuessTheFlag/ContentView.swift
@@ -8,6 +8,20 @@
 
 import SwiftUI
 
+fileprivate let labels = [
+    "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+    "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+    "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+    "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+    "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+    "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+    "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+    "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+    "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+    "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+    "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner",
+]
+
 struct FlagView: View {
     let name: String
     let correct: Bool
@@ -40,6 +54,7 @@ struct FlagView: View {
                 .shadow(color: .black, radius: 2)
                 .rotation3DEffect(Angle(degrees: (choosen && correct) ? 360.0 : 0.0), axis: (x: 0, y: 1, z: 0))
                 .animation(enabled ? nil : Animation.easeOut.delay(0.5))
+                .accessibility(label: Text(labels[name, default: "Unknown flag"]))
         }
         .disabled(!enabled)
     }
