65951dbb8ef213a9a3f6fb2461d717a6f81c35f5
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-12-15 13:11:17 +0300

    improve better rest

--- project_04/BetterRest/ContentView.swift
+++ project_04/BetterRest/ContentView.swift
@@ -84,6 +84,8 @@ struct ContentView: View {
                         Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                             Text("\(sleepAmount, specifier: "%g") hours")
                         }
+                        .accessibility(label: Text("Amount of sleep"))
+                        .accessibility(value: Text("\(sleepAmount, specifier: "%g") hours"))
                     }
 
                     VStack {
@@ -112,6 +114,7 @@ struct ContentView: View {
                             .font(.largeTitle)
                     }
                     .frame(maxWidth: .infinity)
+                    .accessibilityElement(children: .combine)
                 }
             }
             .padding()
